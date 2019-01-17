5b401c5a75c88b6c06dc790a61a2bd9d6d0281da
GellertSiskovits <geraldlionb@gmail.com>
2019-01-17 09:17:22 +0000

    [master] initial

---
+++ webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,13 @@
+/* global QUnit */
+
+QUnit.config.autostart = false;
+
+sap.ui.getCore().attachInit(function() {
+	"use strict";
+
+	sap.ui.require([
+		"sap/com/mobileApp/test/integration/AllJourneys"
+	], function() {
+		QUnit.start();
+	});
+});
